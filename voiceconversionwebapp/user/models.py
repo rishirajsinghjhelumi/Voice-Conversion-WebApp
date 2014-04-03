ec23cdd4609abb0ed0108c7b990406ff7e0d18a8
rishirajsinghjhelumi <rishiakhnoor@gmail.com>
2014-04-04 02:09:17 +0530

    Add user Model, login,logout and register controllers.

---
+++ voiceconversionwebapp/user/models.py
@@ -0,0 +1,87 @@
+from sqlalchemy import Column,Integer,Boolean
+
+from ..models import Base,DBSession
+
+from sqlalchemy.types import String
+from sqlalchemy.schema import ForeignKey
+from sqlalchemy.orm import relationship
+
+from ..speech_text.models import Paragraph
+
+class User(Base):
+    
+    __tablename__ = 'users'
+    
+    id = Column(Integer,primary_key=True)
+    name = Column(String(256))
+    email = Column(String(256),nullable = False)
+    password = Column(String(256))
+    profile_pic = Column(String(4096))
+    
+    def __init__(self, name, email, password, profile_pic):
+        
+        self.name = name
+        self.email = email
+        self.profile_pic = profile_pic
+        self.password = password
+        
+    def getJSON(self):
+        
+        jsonDict = {}
+        jsonDict['id'] = self.id
+        jsonDict['name'] = self.name
+        jsonDict['email'] = self.email
+        jsonDict['profile_pic'] = self.profile_pic
+        
+        return jsonDict
+
+class UserProperty(Base):
+
+    __tablename__ = 'user_properties'
+
+    id = Column(Integer,primary_key=True)
+    user_id = Column(Integer,ForeignKey('users.id'),default = 1)
+    #user = relationship("User",foreign_keys=[user_id])
+
+    completed_training = Column(Boolean)
+    paragraph_read_count = Column(Integer)
+
+    def __init__(self, user_id, completed_training = False, paragraph_read_count = 0):
+
+        self.user_id = user_id
+        self.completed_training = completed_training
+        self.paragraph_read_count = paragraph_read_count
+
+    def getJSON(self):
+
+        jsonDict = {}
+        jsonDict['user_id'] = self.user_id
+        jsonDict['completed_training'] = self.completed_training
+        jsonDict['paragraph_read_count'] = self.paragraph_read_count
+
+        return jsonDict
+
+class UserParagraph(Base):
+
+    __tablename__ = 'user_paragraphs'
+
+    id = Column(Integer,primary_key=True)
+    user_id = Column(Integer,ForeignKey('users.id'),default = 1)
+    #user = relationship("User",foreign_keys=[user_id])
+
+    paragraph_id = Column(Integer,ForeignKey('paragraphs.id'),default = 1)
+    #paragraph = relationship("Paragraph",foreign_keys=[paragraph_id])
+
+    def __init__(self, user_id, paragraph_id):
+
+        self.user_id = user_id
+        self.paragraph_id = paragraph_id
+
+    def getJSON(self):
+
+        jsonDict = {}
+        jsonDict['id'] = self.id
+        jsonDict['user_id'] = self.user_id
+        jsonDict['paragraph_id'] = self.paragraph_id
+
+        return jsonDict
---
+++ voiceconversionwebapp/user/views.py
@@ -0,0 +1,84 @@
+from pyramid.response import Response
+from pyramid.view import view_config, forbidden_view_config
+from pyramid.security import remember,authenticated_userid, forget, Authenticated
+
+from pyramid.httpexceptions import HTTPFound
+
+import hashlib
+
+from .models import DBSession
+from .models import User, UserProperty
+
+from sqlalchemy import and_
+
+_DEFAULT_PROFILE_PIC = ""
+
+def initUserDirectories():
+	pass
+
+@view_config(
+	route_name='register',
+	renderer='json',
+	request_method='POST',
+	permission='__no_permission_required__'
+)
+def register(request):
+
+	name = request.POST['name']
+	email = request.POST['email']
+	password = hashlib.sha256(request.POST['password']).hexdigest()
+	# profile_pic = request.POST['profile_pic']
+
+	dbFoundUser = DBSession.query(User).filter(User.email == email).first()
+	if dbFoundUser:
+		return {'status' : 'false'}
+
+	dbFoundUser = User(name, email, password, _DEFAULT_PROFILE_PIC)
+	DBSession.add(dbFoundUser)
+	DBSession.flush()
+
+	userProperty = UserProperty(dbFoundUser.id)
+	DBSession.add(userProperty)
+	DBSession.flush()
+
+	initUserDirectories()
+
+	request.session['user'] = dbFoundUser.getJSON()
+	headers = remember(request, dbFoundUser.id)
+
+	return HTTPFound(location = request.route_url('home'), headers = headers)
+
+@view_config(
+	route_name='login',
+	renderer='json',
+	request_method='POST',
+	permission='__no_permission_required__'
+)
+def login(request):
+
+	email = request.POST['email']
+	password = hashlib.sha256(request.POST['password']).hexdigest()
+
+	dbFoundUser = DBSession.query(User).\
+	filter(and_(User.email == email, User.password == password)).\
+	first()
+
+	if dbFoundUser is None:
+		return {'status' : 'false'}
+
+	request.session['user'] = dbFoundUser.getJSON()
+	headers = remember(request,dbFoundUser.id)
+
+	return HTTPFound(location = request.route_url('home'), headers = headers)
+
+@view_config(
+	route_name='logout',
+	renderer='json'
+)
+def logout(request):
+    
+    currentUser = int(authenticated_userid(request))
+    headers = forget(request)
+    request.session.invalidate()
+
+    return HTTPFound(location = request.route_url('home'), headers = headers)
