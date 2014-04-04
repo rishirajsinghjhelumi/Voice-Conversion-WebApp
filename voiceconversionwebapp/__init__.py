a1ec863ef67bba72499f6d1567af0db7020da2fe
rishirajsinghjhelumi <rishiakhnoor@gmail.com>
2014-04-04 18:41:20 +0530

    Add TrainWith controller.

--- voiceconversionwebapp/__init__.py
+++ voiceconversionwebapp/__init__.py
@@ -73,6 +73,8 @@ def main(global_config, **settings):
 
     config.add_route('getAllTrainedUsers','/get_all_trained_users')
     config.add_route('getUsersTrainedWith','/get_users_trained_with')
+
+    config.add_route('trainWith','/train_with/{user_id}')
     
     config.scan()
     return config.make_wsgi_app()
---
+++ voiceconversionwebapp/training/views.py
@@ -0,0 +1,52 @@
+from pyramid.response import Response
+from pyramid.view import view_config, forbidden_view_config
+from pyramid.security import remember,authenticated_userid, forget, Authenticated
+
+from pyramid.httpexceptions import HTTPFound
+
+from sqlalchemy import and_
+
+from .models import DBSession
+from .models import TrainedCouple
+from ..user.models import User, UserParagraph, UserProperty
+
+@view_config(
+	route_name='trainWith',
+	renderer='json',
+	request_method='GET'
+)
+def trainWith(request):
+
+	currentUser = int(authenticated_userid(request))
+	userId = int(request.matchdict['user_id'])
+
+	trained = DBSession.query(TrainedCouple).\
+	filter(and_(TrainedCouple.user1_id == currentUser, TrainedCouple.user2_id == userId)).\
+	first()
+
+	if trained:
+		return {'status' : 'Already Trained' }
+
+	currentUserTrainingComplete = DBSession.query(UserProperty.completed_training).\
+	filter(UserProperty.user_id == currentUser).\
+	first()
+
+	if currentUserTrainingComplete == False:
+		return {'status' : 'You have not completed training. Please do it first!!!!'}
+
+	userTrainingComplete = DBSession.query(UserProperty.completed_training).\
+	filter(UserProperty.user_id == userId).\
+	first()
+
+	if userTrainingComplete == False:
+		return {'status' : 'The other user has not yet completed training!!!!'}
+
+	# train_couple(currentUser, userId) #TODO
+
+	trainedCouple = TrainedCouple(currentUser, userId)
+	DBSession.add(trainedCouple)
+	trainedCouple = TrainedCouple(userId, currentUser)
+	DBSession.add(trainedCouple)
+	DBSession.flush()
+
+	return {'status' : 'Training Complete'}
